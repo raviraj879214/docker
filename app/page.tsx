8c76208feca10a5c3f7a69eb7087a63cebdd9149
raviraj879214 <ravirajalphainfo@gmail.com>
2026-03-03 13:25:39 +0530

    databse url

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div>
       API URL Raviraj : {process.env.NEXT_PUBLIC_API_URL}
+      DATABASE URL Raviraj : {process.env.DATABASE_URL}
     </div>
   );
 }
